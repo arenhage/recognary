43879a386c2583100cdb109997e0209ffdca2f74
dunnless <daniel.arenhage@gmail.com>
2013-06-25 23:20:46 +0200

    reattached the seekbar for a easier handle on settings

--- Recognary/src/se/swg/recognary/MainActivity.java
+++ Recognary/src/se/swg/recognary/MainActivity.java
@@ -48,6 +48,15 @@ public class MainActivity extends Activity {
 	private String TAG = "RECOGNARY";
 
 	Dialog settingsDialog;
+	
+	SeekBar seekBar_lower_h;
+	SeekBar seekBar_lower_s;
+	SeekBar seekBar_lower_v;
+
+	SeekBar seekBar_upper_h;
+	SeekBar seekBar_upper_s;
+	SeekBar seekBar_upper_v;
+	
 	Button btn_minus_lower_h;
 	Button btn_minus_lower_s;
 	Button btn_minus_lower_v;
@@ -85,12 +94,24 @@ public class MainActivity extends Activity {
 		settingsDialog.setTitle("HSV Settings");
 		settingsDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 		//move the dialog to horizontal most right
-//		Window window = settingsDialog.getWindow();
-//		WindowManager.LayoutParams wlp = window.getAttributes();
-//		wlp.gravity = Gravity.RIGHT;
-//		wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-//		window.setAttributes(wlp);
+		Window window = settingsDialog.getWindow();
+		WindowManager.LayoutParams wlp = window.getAttributes();
+		wlp.gravity = Gravity.RIGHT;
+		wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+		window.setAttributes(wlp);
 		
+		seekBar_lower_h = (SeekBar)layout.findViewById(R.id.seekBar_lower_h);
+		seekBar_lower_s = (SeekBar)layout.findViewById(R.id.seekBar_lower_s);
+		seekBar_lower_v = (SeekBar)layout.findViewById(R.id.seekBar_lower_v);
+		seekBar_upper_h = (SeekBar)layout.findViewById(R.id.seekBar_upper_h);
+		seekBar_upper_s = (SeekBar)layout.findViewById(R.id.seekBar_upper_s);
+		seekBar_upper_v = (SeekBar)layout.findViewById(R.id.seekBar_upper_v);
+		seekBar_lower_h.setOnSeekBarChangeListener(seekBar_lower_h_listener);
+		seekBar_lower_s.setOnSeekBarChangeListener(seekBar_lower_s_listener);
+		seekBar_lower_v.setOnSeekBarChangeListener(seekBar_lower_v_listener);
+		seekBar_upper_h.setOnSeekBarChangeListener(seekBar_upper_h_listener);
+		seekBar_upper_s.setOnSeekBarChangeListener(seekBar_upper_s_listener);
+		seekBar_upper_v.setOnSeekBarChangeListener(seekBar_upper_v_listener);
 		
 		btn_minus_lower_h = (Button)layout.findViewById(R.id.btn_minus_lower_h);
 		btn_minus_lower_s = (Button)layout.findViewById(R.id.btn_minus_lower_s);
@@ -119,9 +140,6 @@ public class MainActivity extends Activity {
 		btn_plus_upper_h.setOnClickListener(btn_plus_upper_h_listener);
 		btn_plus_upper_s.setOnClickListener(btn_plus_upper_s_listener);
 		btn_plus_upper_v.setOnClickListener(btn_plus_upper_v_listener);
-		
-		btn_minus_lower_h.setOnClickListener(btn_minus_lower_h_listener);
-
 	}
 	
 	//H=1, S=2, V=3
@@ -133,26 +151,30 @@ public class MainActivity extends Activity {
 			if(number-1 >= 0) {
 				changeHSVbound(1,1,number-1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_h_value)).setText((number-1)+"");
+				seekBar_lower_h.setProgress(progress_lower_h = number-1);
 			}
-			
 		}
 	};
+	
 	OnClickListener btn_minus_lower_s_listener = new OnClickListener() {
 		public void onClick(View v) {
 			int number = Integer.parseInt(((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_s_value)).getText().toString());
 			if(number-1 >= 0) {
 				changeHSVbound(1,2,number-1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_s_value)).setText((number-1)+"");
+				seekBar_lower_s.setProgress(progress_lower_s = number-1);
 			}
 			
 		}
 	};	
+	
 	OnClickListener btn_minus_lower_v_listener = new OnClickListener() {
 		public void onClick(View v) {
 			int number = Integer.parseInt(((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_v_value)).getText().toString());
 			if(number-1 >= 0) {
 				changeHSVbound(1,3,number-1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_v_value)).setText((number-1)+"");
+				seekBar_lower_v.setProgress(progress_lower_v = number-1);
 			}
 			
 		}
@@ -164,16 +186,19 @@ public class MainActivity extends Activity {
 			if(number+1 <= 180) {
 				changeHSVbound(1,1,number+1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_h_value)).setText((number+1)+"");
+				seekBar_lower_h.setProgress(progress_lower_h = number+1);
 			}
 			
 		}
 	};
+	
 	OnClickListener btn_plus_lower_s_listener = new OnClickListener() {
 		public void onClick(View v) {
 			int number = Integer.parseInt(((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_s_value)).getText().toString());
 			if(number+1 <= 255) {
 				changeHSVbound(1,2,number+1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_s_value)).setText((number+1)+"");
+				seekBar_lower_s.setProgress(progress_lower_s = number+1);
 			}
 			
 		}
@@ -185,6 +210,7 @@ public class MainActivity extends Activity {
 			if(number+1 <= 255) {
 				changeHSVbound(1,2,number+1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_v_value)).setText((number+1)+"");
+				seekBar_lower_v.setProgress(progress_lower_v = number+1);
 			}
 			
 		}
@@ -196,6 +222,7 @@ public class MainActivity extends Activity {
 			if(number-1 >= 0) {
 				changeHSVbound(1,1,number-1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.upper_h_value)).setText((number-1)+"");
+				seekBar_upper_h.setProgress(progress_upper_h = number-1);
 			}
 			
 		}
@@ -207,6 +234,7 @@ public class MainActivity extends Activity {
 			if(number-1 >= 0) {
 				changeHSVbound(1,2,number-1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.upper_s_value)).setText((number-1)+"");
+				seekBar_upper_s.setProgress(progress_upper_s = number-1);
 			}
 			
 		}
@@ -218,6 +246,7 @@ public class MainActivity extends Activity {
 			if(number-1 >= 0) {
 				changeHSVbound(1,3,number-1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.upper_v_value)).setText((number-1)+"");
+				seekBar_upper_v.setProgress(progress_upper_v = number-1);
 			}
 			
 		}
@@ -229,6 +258,7 @@ public class MainActivity extends Activity {
 			if(number+1 <= 180) {
 				changeHSVbound(1,1,number+1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.upper_h_value)).setText((number+1)+"");
+				seekBar_upper_h.setProgress(progress_upper_h = number+1);
 			}
 			
 		}
@@ -240,6 +270,7 @@ public class MainActivity extends Activity {
 			if(number+1 <= 255) {
 				changeHSVbound(1,2,number+1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.upper_s_value)).setText((number+1)+"");
+				seekBar_upper_s.setProgress(progress_upper_s = number+1);
 			}
 			
 		}
@@ -251,8 +282,91 @@ public class MainActivity extends Activity {
 			if(number+1 <= 255) {
 				changeHSVbound(1,2,number+1);
 				((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.upper_v_value)).setText((number+1)+"");
+				seekBar_upper_v.setProgress(progress_upper_v = number+1);
 			}
 			
+		}
+	};
+	
+	OnSeekBarChangeListener seekBar_lower_h_listener = new OnSeekBarChangeListener() {
+		public void onProgressChanged(SeekBar seekBark, int progress, boolean fromUser) {
+			Log.i(TAG, "VALUE LOWER H:" + progress);
+			progress_lower_h = progress;
+		}
+
+		public void onStartTrackingTouch(SeekBar seekBar) {}
+
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			changeHSVbound(1,1,progress_lower_h);	//lower,H,progress
+			((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_h_value)).setText(progress_lower_h+"");
+		}
+	};
+
+	OnSeekBarChangeListener seekBar_lower_s_listener = new OnSeekBarChangeListener() {
+		public void onProgressChanged(SeekBar seekBark, int progress, boolean fromUser) {
+			Log.i(TAG, "VALUE LOWER S:" + progress);
+			progress_lower_s = progress;
+		}
+
+		public void onStartTrackingTouch(SeekBar seekBar) {}
+
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			changeHSVbound(1,2,progress_lower_s);	//lower,S,progress
+			((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_s_value)).setText(progress_lower_s+"");
+		}
+	};
+
+	OnSeekBarChangeListener seekBar_lower_v_listener = new OnSeekBarChangeListener() {
+		public void onProgressChanged(SeekBar seekBark, int progress, boolean fromUser) {
+			Log.i(TAG, "VALUE LOWER V:" + progress);
+			progress_lower_v = progress;
+		}
+
+		public void onStartTrackingTouch(SeekBar seekBar) {}
+
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			changeHSVbound(1,3,progress_lower_v);	//lower,V,progress
+			((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.lower_v_value)).setText(progress_lower_v+"");
+		}
+	};
+	
+	OnSeekBarChangeListener seekBar_upper_h_listener = new OnSeekBarChangeListener() {
+		public void onProgressChanged(SeekBar seekBark, int progress, boolean fromUser) {
+			Log.i(TAG, "VALUE upper H:" + progress);
+			progress_upper_h = progress;
+		}
+		public void onStartTrackingTouch(SeekBar seekBar) {}
+
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			changeHSVbound(2,1,progress_upper_h);	//upper,H,progress
+			((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.upper_h_value)).setText(progress_upper_h+"");
+		}
+	};
+	
+	OnSeekBarChangeListener seekBar_upper_s_listener = new OnSeekBarChangeListener() {
+		public void onProgressChanged(SeekBar seekBark, int progress, boolean fromUser) {
+			Log.i(TAG, "VALUE upper S:" + progress);
+			progress_upper_s = progress;
+		}
+		public void onStartTrackingTouch(SeekBar seekBar) {}
+
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			changeHSVbound(2,2,progress_upper_s);	//upper,S,progress
+			((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.upper_s_value)).setText(progress_upper_s+"");
+		}
+	};
+	
+	OnSeekBarChangeListener seekBar_upper_v_listener = new OnSeekBarChangeListener() {
+		public void onProgressChanged(SeekBar seekBark, int progress, boolean fromUser) {
+			Log.i(TAG, "VALUE upper V:" + progress);
+			progress_upper_v = progress;
+		}
+
+		public void onStartTrackingTouch(SeekBar seekBar) {}
+
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			changeHSVbound(2,3,progress_upper_v);	//upper,V,progress
+			((TextView)settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content).findViewById(R.id.upper_v_value)).setText(progress_upper_v+"");
 		}
 	};
 	
@@ -314,6 +428,12 @@ public class MainActivity extends Activity {
 	}
 	
 	public void resetDialogSeekbar() {
+		seekBar_lower_h.setProgress(170);
+		seekBar_lower_s.setProgress(160);
+		seekBar_lower_v.setProgress(60);
+		seekBar_upper_h.setProgress(180);
+		seekBar_upper_s.setProgress(255);
+		seekBar_upper_v.setProgress(255);
 		View dialogContent = settingsDialog.getWindow().getDecorView().findViewById(android.R.id.content);
 		((TextView)dialogContent.findViewById(R.id.lower_h_value)).setText(170+"");
 		((TextView)dialogContent.findViewById(R.id.lower_s_value)).setText(160+"");
